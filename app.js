58d72f6cbeae65412c381dac5ff85ff3578c036b
thuang3316 <249622692+thuang3316@users.noreply.github.com>
2026-02-08 19:12:28 -0500

    v1.0

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require("node:path");
+const adminRouter = require('./routes/adminRouter');
+const homeRouter = require('./routes/homeRouter');
+
+const app = express();
+const port = 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use('/admin', adminRouter);
+app.use('/home', homeRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`App running @ http://localhost:${port}`);
+})
